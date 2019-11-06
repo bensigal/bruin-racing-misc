f915dc3339bc64b43c8760c43ca0f24ebd351869
bensigal <ben@bensigal.com>
2019-11-05 20:38:25 -0800

    Commented HalifaxTest, fixed error in circumference calculation.

--- HalifaxTest/HalifaxTest.ino
+++ HalifaxTest/HalifaxTest.ino
@@ -1,12 +1,15 @@
 #define INPUT_PIN A0
 #define THRESHOLD 100
-//0: Just the voltage, 1: Every time it changes, 2: Speed
+
+//0: Print out the analog value read, 0-1023.
+//1: Print whenever it switches from a low signal (something's there) to a high (nothing's there)
+//2: Count how many times it gets a high signal. Based on that, calculate frequency and speed.
 #define MODE 2
 
 //In ms
 #define SPEED_MEASUREMENT_INTERVAL 5000
 //In in
-#define CIRCUMFERENCE 2*PI*18
+#define CIRCUMFERENCE PI*18
 
 void setup() {
   Serial.begin(9600);
@@ -24,18 +27,24 @@ void loop() {
   #elif MODE==1
   
     Serial.println("High");
+    //Wait until signal does not exceed threshold
     while(analogRead(INPUT_PIN) > THRESHOLD){
       ;
     }
     Serial.println("Low");
+    //Wait until signal exceeds threshold
     while(analogRead(INPUT_PIN <= THRESHOLD)){
       ;
     }
     
   #elif MODE==2
-  
+
+    //Get current time
     int startTick = millis();
+    //Count number of times a high signal is reached
     int highs = 0;
+    //While it's been less than SPEED_MEASUREMENT_INTERVAL ms...
+    //Note: because this loop involves waiting, it will probably be more than that long. This is accounted for.
     while(millis() < startTick + SPEED_MEASUREMENT_INTERVAL){
       while(analogRead(INPUT_PIN) > THRESHOLD){
         ;
